7804c54f92319074c792609cf61ee08248d82b55
TrustfratedIsNotAvailable <nusratmithila524@gmail.com>
2025-03-02 19:10:55 +0600

    clear.js added

---
+++ script/clear.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-clear").addEventListener('click',function(){
+     const historyContainer = document.getElementById("history-container");
+     historyContainer.style.display = "none";
+})
